1433b92aa724fc66ab9fe4743a7c797ecf3b54c3
JOSUÉ ZEPEDA (ZF19001) <zf19001@ues.edu.sv>
2022-10-06 18:34:24 -0600

    Corrección de Echo
    Cambio total del codigo Echo, modificacion de lo que deberia de hacer el trabajo en si

--- Echo/dist/index.js
+++ Echo/dist/index.js
@@ -1,16 +1,2 @@
-"use strict";
-exports.__esModule = true;
-var fs = require("fs");
-// En esta parte pones lo que tu quieres para mostrar
-//                           |
-//                           V
-var parametro = "AQUI VA EL PARAMETRO A COPIAR";
-// este archivo se usará para mostrar el texto
-var archivoparametro = "parametro.txt";
-// se copia en el archivo el texto que se ha ingresado en la variable parámetro
-fs.writeFileSync(archivoparametro, parametro);
-var archivoparametrocontenido = fs.readFileSync(archivoparametro, 'utf8');
-console.log(archivoparametrocontenido);
-//vacio el archivo para próximos usos
-var parametroreset = "";
-fs.writeFileSync(archivoparametro, parametroreset);
+var argsArray = process.argv.slice(2);
+console.info(argsArray.join(" "));
--- Echo/src/index.ts
+++ Echo/src/index.ts
@@ -1,14 +1,4 @@
-import * as fs from 'fs';
-// En esta parte pones lo que tu quieres para mostrar
-//                           |
-//                           V
-let parametro: string="AQUI VA EL PARAMETRO A COPIAR"
-// este archivo se usará para mostrar el texto
-const archivoparametro: string = "parametro.txt";
-// se copia en el archivo el texto que se ha ingresado en la variable parámetro
-fs.writeFileSync(archivoparametro, parametro);
-let archivoparametrocontenido = fs.readFileSync(archivoparametro, 'utf8')
-console.log(archivoparametrocontenido)
-//vacio el archivo para próximos usos
-let parametroreset: string="";
-fs.writeFileSync(archivoparametro, parametroreset)
+// CORRECCIÓN DE ECHO BASADO EN LO VISTO EN LA INSTRUCTORIA DE 6
+// DE OCTUBRE DE 2022
+const Arraydelcliente = process.argv.slice(2);
+console.info(Arraydelcliente.join(" "));
